c12f1baebd40f748fff66bd912e411d30923733b
luddthree <luddetv@gmail.com>
2025-02-27 10:09:36 +0100

    sss

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3001')],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3001')],
+    // 'allowed_origins' => ['http://localhost:3000'], // Remove '*'
+    // 'allowed_origins' => ['*'],
 
 
     'allowed_origins_patterns' => [],
